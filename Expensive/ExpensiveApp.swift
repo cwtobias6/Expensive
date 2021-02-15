66caf5d97a479c079375ca001e4de18669e981f3
Christian Tobias <cwtobias6@gmail.com>
2021-02-14 21:14:29 -0800

    Initial Commit

---
+++ Expensive/ExpensiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensiveApp.swift
+//  Expensive
+//
+//  Created by Christian Tobias on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct ExpensiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
